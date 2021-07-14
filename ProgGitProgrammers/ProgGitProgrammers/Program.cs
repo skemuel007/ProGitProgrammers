09d2d4c2069a468f6ec03475ee582459520bbabd
Salvation <skemuel007@gmail.com>
2021-07-14 20:36:29 +0100

    My Message

--- ProgGitProgrammers/ProgGitProgrammers/Program.cs
+++ ProgGitProgrammers/ProgGitProgrammers/Program.cs
@@ -9,6 +9,7 @@ namespace ProgGitProgrammers
             Console.WriteLine("Hello World!");
             Console.WriteLine("I just added this in Visual Studio");
             Console.WriteLine("I just added this in the command line repo");
+            Console.WriteLine("This line added to show the staging area");
         }
     }
 }
