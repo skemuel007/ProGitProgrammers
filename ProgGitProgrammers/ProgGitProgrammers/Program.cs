3e764d3bd2473eb90d2ab9939c342925165da099
Salvation <skemuel007@gmail.com>
2021-07-14 20:13:20 +0100

    Add writeline indicating we are in command line

--- ProgGitProgrammers/ProgGitProgrammers/Program.cs
+++ ProgGitProgrammers/ProgGitProgrammers/Program.cs
@@ -8,6 +8,7 @@ namespace ProgGitProgrammers
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("I just added this in Visual Studio");
+            Console.WriteLine("I just added this in the command line repo");
         }
     }
 }
